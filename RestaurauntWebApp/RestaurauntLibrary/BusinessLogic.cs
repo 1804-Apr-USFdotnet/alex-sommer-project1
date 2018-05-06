2b9f20c9a591f4263c0dd73b7f65b661e54bf387
alexBSommer <alex.b.sommer@gmail.com>
2018-05-06 19:27:30 -0400

    Serialization and buisiness top three method

--- RestaurauntWebApp/RestaurauntLibrary/BusinessLogic.cs
+++ RestaurauntWebApp/RestaurauntLibrary/BusinessLogic.cs
@@ -35,5 +35,48 @@ namespace RestaurauntLibrary
             }
             return results;
         }
+
+        public static List<RestaurauntLibrary.Restauraunt> TopThree(List<RestaurauntLibrary.Restauraunt> candidates)
+        {
+            List<Restauraunt> results = new List<Restauraunt>();
+            List<Restauraunt> workspace = candidates;
+            float Best = 0F;
+            Restauraunt top = null;
+            foreach (Restauraunt r in workspace)
+            {
+                if (r.GetAverage() > Best)
+                {
+                    Best = r.GetAverage();
+                    top = r;
+                }
+            }
+            workspace.Remove(top);
+            float SecondBest = 0F;
+            Restauraunt second = null;
+            foreach (Restauraunt r in workspace)
+            {
+                if (r.GetAverage() > SecondBest)
+                {
+                    SecondBest = r.GetAverage();
+                    second = r;
+                }
+            }
+            workspace.Remove(second);
+            float ThirdBest = 0F;
+            Restauraunt third = null;
+            foreach (Restauraunt r in workspace)
+            {
+                if (r.GetAverage() > ThirdBest)
+                {
+                    ThirdBest = r.GetAverage();
+                    third = r;
+                }
+            }
+            results.Add(top);
+            results.Add(second);
+            results.Add(third);
+
+            return results;
+        }
     }
 }
---
+++ RestaurauntWebApp/RestaurauntLibrary/Serializer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace RestaurauntLibrary
+{
+    public static class Serializer
+    {
+        public static string RestaurauntToString(Restauraunt target)
+        {
+
+            MemoryStream ms = new MemoryStream();
+            string jsonString;
+            try
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Restauraunt));
+                ms.Position = 0;
+                StreamReader sr = new StreamReader(ms);
+                ser.WriteObject(ms, target);
+                ms.Position = 0;
+                jsonString = sr.ReadToEnd();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                ms.Close();
+            }
+            return jsonString;
+        }
+        public static Restauraunt StringToRestauraunt(string jsonString)
+        {
+            //deserializer goes here
+            Restauraunt deserializedRestauraunt;
+            MemoryStream ms = new MemoryStream();
+            try
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Restauraunt));
+                byte[] bytes = Encoding.ASCII.GetBytes(jsonString);
+                ms.Write(bytes, 0, Encoding.ASCII.GetByteCount(jsonString));
+                ms.Position = 0;
+                deserializedRestauraunt = (Restauraunt)ser.ReadObject(ms);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                ms.Close();
+            }
+            return deserializedRestauraunt;
+        }
+        public static List<Restauraunt> GetRestaraunts(string json)
+        {
+            using (Stream stream = File.OpenRead(json))
+            {
+                var ser = new DataContractJsonSerializer(typeof(RestarauntList));
+                RestarauntList list = (RestarauntList)ser.ReadObject(stream);
+                return list.restaraunts;
+            }
+
+        }
+        [DataContract]
+        public class RestarauntList
+        {
+            [DataMember]
+            public List<Restauraunt> restaraunts { get; set; }
+        }
+    }
+}
